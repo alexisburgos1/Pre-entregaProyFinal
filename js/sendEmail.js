5e90949f612c0709b8224de02e1812e101b3dd52
alexisburgos1 <107444107+alexisburgos1@users.noreply.github.com>
2022-10-06 17:41:13 -0300

    adding sendEmail.js

---
+++ js/sendEmail.js
@@ -0,0 +1,23 @@
+function sendEmail(){ 
+    
+    const btn = document.getElementById('SendEmailId');
+    document.getElementById('formEmail')
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        
+        btn.value = 'Sending...';
+        
+
+        const serviceID = 'default_service';
+        const templateID = 'template_44gzlvk';
+        
+        emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Thanks for your message!');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+    });}
